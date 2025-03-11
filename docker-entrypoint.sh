e553c86489d9d3f89924891385fe2eca99182af4
dsma96 <silverwing@gmail.com>
2025-03-10 23:52:50 -0400

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-export DB_PWD=!DORmdssws00
-export DB_USR=dorothy
-export DB_URL=jdbc:mariadb://192.168.50.130:3306/dorothy
-export JWT_SECRET=DorothyDorothyDorothyDorothyDorothyDorothyDorothy
-
 exec java -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=cloud -jar /app.jar
